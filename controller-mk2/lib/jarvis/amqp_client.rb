71861caca4cde8137719081972e8911c09bfccca
Jon Wood <jon@ninjagiraffes.co.uk>
2014-10-23 14:29:31 +0100

    Make this deployable
    * Retry on connection failure
    * Log to syslog

--- controller-mk2/lib/jarvis/amqp_client.rb
+++ controller-mk2/lib/jarvis/amqp_client.rb
@@ -1,15 +1,12 @@
 require 'jarvis/version'
-require 'logger'
+require 'syslog/logger'
 require 'bunny'
 
 module Jarvis
   class AmqpClient
     class Logger
-      def initialize(target = $stdout)
-        @logger = ::Logger.new(target)
-        @logger.formatter = ->(severity, time, progname, msg) {
-          "#{msg} version=#{Jarvis::VERSION} time=#{time.strftime("%FT%T%z")} severity=#{severity}\n"
-        }
+      def initialize(name)
+        @logger = Syslog::Logger.new(name)
       end
 
       def log(level, message, attributes = {})
@@ -17,14 +14,32 @@ module Jarvis
       end
     end
     
-    def self.run
-      logger = Logger.new
-      logger.log(:info, "Starting Jarvis Controller")
-      logger.log(:debug, "Establishing RabbitMQ connection", uri: ENV["RABBITMQ_URL"], bunny_version: Bunny::VERSION)
-      
-      Bunny.run do |client|
-        logger.log(:debug, "Established RabbitMQ connection", uri: ENV["RABBITMQ_URL"], bunny_version: Bunny::VERSION)
-        yield client, logger
+    def self.run(name = nil)
+      if name.nil?
+        name = File.basename($0)
+      end
+
+      logger = Logger.new(name)
+      logger.log(:info, "Starting #{name}")
+
+      retry_count = 0
+
+      begin
+        logger.log(:debug, "Establishing RabbitMQ connection", uri: ENV["RABBITMQ_URL"], bunny_version: Bunny::VERSION)
+        Bunny.run do |client|
+          logger.log(:debug, "Established RabbitMQ connection", uri: ENV["RABBITMQ_URL"], bunny_version: Bunny::VERSION)
+          yield client, logger
+        end
+      rescue Bunny::TCPConnectionFailed => e
+        if retry_count > 4
+          logger.log(:fatal, "Could not connect to RabbitMQ after 5 attempts, giving up", uri: ENV["RABBITMQ_URL"], bunny_version: Bunny::VERSION, attempt: retry_count)
+          exit 1
+        else
+          retry_count += 1
+          logger.log(:warn, "RabbitMQ connection failed, retrying", uri: ENV["RABBITMQ_URL"], bunny_version: Bunny::VERSION, attempt: retry_count)
+          sleep 5
+          retry
+        end
       end
     end
   end
